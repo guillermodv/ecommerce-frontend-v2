43da37149ebdfb6ee51a2035d828652dee7224c9
guillermogdv <guillermo.delvecchio@wegotcha.io>
2024-07-09 13:43:24 -0300

    first fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 /* eslint-disable @next/next/no-img-element */
 export default function Home() {
   return (
@@ -12,7 +10,6 @@ export default function Home() {
           <p className="text-center pb-3 text-black">
             Aquí encontrarás las mejores marcas y productos de la tienda.
           </p>
-          <img src="https://www.moov.com.ar/dw/image/v2/BDTF_PRD/on/demandware.static/-/Sites-Moov-Library/default/dw19b720d7/25sept/full3adidasbc.jpg?sw=1920&sfrm=jpg&q=90" />
         </div>
       </section>
     </main>
